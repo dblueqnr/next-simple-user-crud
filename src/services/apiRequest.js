2a4d0fd1e1a387458c2aa44218b8c703bc25983e
Daniel Blumer <danblumer@gmail.com>
2021-03-01 16:17:19 -0300

    change the environment api url
    change the environment api url

--- src/services/apiRequest.js
+++ src/services/apiRequest.js
@@ -1,27 +1,29 @@
+const url = process.env.NEXT_PUBLIC_API_URL;
 export async function loadUsers() {
-  const response = await fetch("http://localhost:3000/api/users/");
+  console.log("aaa", process.env.DATABASE_URL);
+  const response = await fetch(url + "/users/");
   let apiData = await response.json();
   return apiData;
 }
 export async function loadUsersById(userId) {
-  const response = await fetch("http://localhost:3000/api/user/" + userId);
+  const response = await fetch(url + "/user/" + userId);
   return await response.json();
 }
 export async function deleteUserById(userId) {
-  const response = await fetch(`http://localhost:3000/api/user/${userId}`, {
+  const response = await fetch(`${url}/user/${userId}`, {
     method: "delete",
     headers: { "Content-type": "application/json;charset=UTF-8" },
   });
 }
 export async function updateUserById(userId, userData) {
-  const response = await fetch(`http://localhost:3000/api/user/${userId}`, {
+  const response = await fetch(`${url}/user/${userId}`, {
     method: "put",
     headers: { "Content-type": "application/json;charset=UTF-8" },
     body: JSON.stringify(userData),
   });
 }
 export async function createUser(userData) {
-  const response = await fetch(`http://localhost:3000/api/users/`, {
+  const response = await fetch(`${url}/users/`, {
     method: "post",
     headers: { "Content-type": "application/json;charset=UTF-8" },
     body: JSON.stringify(userData),
