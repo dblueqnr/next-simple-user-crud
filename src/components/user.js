6a35c094ad9899c71095d9c261f501988b1c6df6
Daniel Blumer <danblumer@gmail.com>
2021-03-01 17:38:04 -0300

    style the code
    style the code

--- src/components/user.js
+++ src/components/user.js
@@ -3,8 +3,29 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { useState, useEffect } from "react";
 import { createUser, updateUserById } from "../services/apiRequest";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "center",
+  },
+  formContainer: {
+    display: "flex",
+    width: "250px",
+    border: "1px solid black",
+    borderRadius: "5px",
+    flexDirection: "column",
+    margin: "50px 0px",
+    padding: "10px 0px",
+    alignItems: "center",
+    rowGap: "10px",
+  },
+}));
 
 function user({ data, handlerSaved }) {
+  const classes = useStyles();
   const [userData, setUserData] = useState({ name: "", email: "" });
   useEffect(() => {
     if (!data) {
@@ -23,23 +44,26 @@ function user({ data, handlerSaved }) {
 
   return (
     <>
-      <form noValidate autoComplete="off">
-        <TextField
-          id="name"
-          label="Nome"
-          value={userData.name}
-          onChange={(e) => setUserData({ ...userData, name: e.target.value })}
-        />
-        <TextField
-          id="email"
-          label="E-Mail"
-          value={userData.email}
-          onChange={(e) => setUserData({ ...userData, email: e.target.value })}
-        />
-        <Button variant="contained" color="primary" onClick={save}>
-          Salvar
-        </Button>
-      </form>
+      <div className={classes.container}>
+        <div className={classes.formContainer}>
+          <label>Cadastrar Usuário</label>
+          <TextField
+            id="name"
+            label="Nome"
+            value={userData.name}
+            onChange={(e) => setUserData({ ...userData, name: e.target.value })}
+          />
+          <TextField
+            id="email"
+            label="E-Mail"
+            value={userData.email}
+            onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+          />
+          <Button variant="contained" color="primary" onClick={save} style={{ width: "50px" }}>
+            Salvar
+          </Button>
+        </div>
+      </div>
     </>
   );
 }
--- src/pages/users.js
+++ src/pages/users.js
@@ -4,6 +4,7 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import Snackbar from "@material-ui/core/Snackbar";
 import Paper from "@material-ui/core/Paper";
 import User from "../components/user";
 import EditIcon from "@material-ui/icons/Edit";
@@ -24,10 +25,26 @@ const useStyles = makeStyles((theme) => ({
     height: "25px",
     cursor: "pointer",
   },
+  tableContainer: {
+    width: "80%",
+    display: "flex",
+    padding: "5px",
+    borderRadius: "5px",
+    backgroundColor: "#cdcccc",
+    flexDirection: "column",
+    rowGap: "10px",
+    boxShadow: "#ece5e5 2px 4px 1px 0px",
+  },
+  viewContainer: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "center",
+  },
 }));
 
 export default function Users({ users }) {
   const [userData, setUserData] = useState(null);
+  const [notification, setNotification] = useState(false);
   const [userList, setUserList] = useState(null);
   const classes = useStyles();
   useEffect(async () => {
@@ -51,6 +68,7 @@ export default function Users({ users }) {
     await deleteUserById(userId);
     setUserData(null);
     load();
+    setNotification(true);
   };
   const editUser = async (userId) => {
     let user = await loadUsersById(userId);
@@ -59,38 +77,60 @@ export default function Users({ users }) {
   const handlerSaved = async () => {
     await load();
     setUserData(null);
+    setNotification(true);
   };
 
   return (
     <>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        open={notification}
+        autoHideDuration={1000}
+        onClose={setNotification}
+        message="Operação realizada com sucesso"
+      />
       <User data={userData} handlerSaved={handlerSaved} />
-      <TableContainer component={Paper}>
-        <Table size="small" aria-label="a dense table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Id</TableCell>
-              <TableCell align="center">Nome</TableCell>
-              <TableCell align="center">E-Mail</TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {userList?.map((user) => (
-              <TableRow key={user.id}>
-                <TableCell component="th" scope="row">
-                  {user.id}
-                </TableCell>
-                <TableCell align="center">{user.name}</TableCell>
-                <TableCell align="center">{user.email}</TableCell>
-                <TableCell align="right">
-                  <EditIcon className={classes.editIcon} onClick={() => editUser(user.id)} />
-                  <RemoveIcon className={classes.removeIcon} onClick={() => removeUser(user.id)} />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <div className={classes.viewContainer}>
+        <div className={classes.tableContainer}>
+          <span>Usuários Cadastrados</span>
+          <TableContainer component={Paper}>
+            <Table size="small" aria-label="a dense table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Id</TableCell>
+                  <TableCell align="center">Nome</TableCell>
+                  <TableCell align="center">E-Mail</TableCell>
+                  <TableCell></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {userList?.map((user) => (
+                  <TableRow key={user.id}>
+                    <TableCell component="th" scope="row">
+                      {user.id}
+                    </TableCell>
+                    <TableCell align="center">{user.name}</TableCell>
+                    <TableCell align="center">{user.email}</TableCell>
+                    <TableCell align="right">
+                      <EditIcon className={classes.editIcon} onClick={() => editUser(user.id)} />
+                      <RemoveIcon className={classes.removeIcon} onClick={() => removeUser(user.id)} />
+                    </TableCell>
+                  </TableRow>
+                ))}
+                {userList == null ||
+                  (userList.length == 0 && (
+                    <TableCell align="center" scope="row" colSpan="4">
+                      Nenhum registro encontrado
+                    </TableCell>
+                  ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      </div>
     </>
   );
 }
